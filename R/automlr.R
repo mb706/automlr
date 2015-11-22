0cd14f557411e8c178a36714107087e022efa3a2
mb706 <mb706@uni>
2015-11-22 15:47:16 +0100

    Starting development of automlr() function.

---
+++ R/automlr.R
@@ -0,0 +1,11 @@
+
+
+
+#' Automatically choose a model with parameters to fit.
+#' 
+#' This is the main entry point of automlr.
+#' @param TODO
+#' @return TODO
+automlr = function() {
+  NULL
+}
--- R/test.R
+++
@@ -1,9 +0,0 @@
-#' Test function
-#' 
-#' This function greets the user and returns TRUE.
-#' @return TRUE
-#' @export 
-test = function() {
-  cat("this worked.\n")
-  TRUE
-}
