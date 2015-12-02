cb982abef4ca26cbc544da8a71fd1bc98ce8afd7
mb706 <mb706@uni>
2015-12-02 14:26:15 +0100

    lsambackends() and isambackend() now work.

--- R/defaults.R
+++ R/defaults.R
@@ -1,3 +1,5 @@
 # Constants and default values should be defined here.
 
 default.save.interval = 300
+
+requiredBackendFunctions = c("combinepriors", "extractprior", "setup", "optimize", "result")
--- R/lsambackends.R
+++ R/lsambackends.R
@@ -1,20 +1,38 @@
 
 #' List the backends currently implemented which can be passed to \code{\link{automlr}}.
 #' 
+#' @param fenv should be NULL
 #' @examples
 #' lsambackends()
 #' @export
-lsambackends = function() {
-  requiredFunctions = c("combinepriors", "extractprior", "setup", "optimize", "result")
+lsambackends = function(fenv=NULL) {
+  # TODO: maybe this weird recursion listing is not a good idea
+  if (is.null(fenv)) {
+    fenv = parent.env(environment())
+  }
+  if (identical(emptyenv(), fenv)) {
+    return(character(0))
+  }
   results = NULL
-  for (rf in requiredFunctions) {
+  for (rf in requiredBackendFunctions) {  # as defined in defaults.R
     pat = paste0("^", rf, "\\.")
-    matches = sub(pat, "", ls(pattern=pat))
+    matches = sub(pat, "", ls(fenv, pattern=pat))
     if (is.null(results)) {
       results = matches
     } else {
       results = intersect(matches, results)
     }
   }
-  matches
+  c(results, lsambackends(parent.env(fenv)))
+}
+
+#' Check if the given name is a valid automlr backend
+#' 
+#' @param name the name of the backend to check for.
+#' @export
+isambackend = function(name) {
+  searchstrings = paste(requiredBackendFunctions, name, sep=".")
+  !any(sapply(mget(searchstrings, inherits=TRUE, mode="function",
+                   ifnotfound=replicate(length(requiredBackendFunctions), NULL)),
+              is.null))
 }
