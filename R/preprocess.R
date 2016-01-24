fbe3c85eda03a9e5f8d4e9a73eeda7b2a89e80e5
mb706 <mb706@uni>
2016-01-24 16:59:32 +0100

    slow but steady progress in preprocess.R

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,22 +1,29 @@
 
-preProcess = function(data, nzv.cutoff.numeric=0, nzv.cutoff.factor, univariate.trafo="off",
+preProcess = function(data, target=NULL, nzv.cutoff.numeric=0, nzv.cutoff.factor=0, univariate.trafo="off",
     impute.numeric="remove.na", impute.factor="remove.na", multivariate.trafo="off", feature.filter="off",
-    feature.filter.thresh = 0) {
+    feature.filter.thresh = 0, DEBUG=FALSE) {
   # first: check arguments
-  assertClass(task, classes = "Task")
+  assertClass(data, classes = "data.frame")
+
   assertNumber(nzv.cutoff.numeric, lower=0)
   assertPercentage(nzv.cutoff.factor)
   assertChoice(univariate.trafo, c("off", "center", "scale", "centerscale", "range"))
   assertChoice(impute.numeric, c("remove.na", "mean", "median", "hist"))
   assertChoice(impute.factor, c("remove.na", "distinct", "mode", "hist"))
   assertChoice(multivariate.trafo, c("off", "pca", "ica"))
-  assertChoice(feature.filter, c("none", "info.gain", "chi.squared", "rf.importance"))
+  assertChoice(feature.filter, c("off", "info.gain", "chi.squared", "rf.importance"))
   if (feature.filter != "off") {
     assertPercentage(feature.filter.thresh)
   }
   
   ppobject = addClasses(as.list(environment()), "ampreproc")  # collect all arguments given
-  
+  ppobject$data = NULL
+
+  hasTarget = is.null(target)
+  if (hasTarget) {
+    targetData = data[[target]]
+    data[target] = NULL
+  }
   # determine the numeric & factor cols
   cols.numeric = sapply(data, is.numeric)
   cols.numeric = names(data)[cols.numeric]
@@ -37,20 +44,24 @@ preProcess = function(data, nzv.cutoff.numeric=0, nzv.cutoff.factor, univariate.
   nzv.drop.factor = ndsplit$`TRUE`
   cols.factor = ndsplit$`FALSE`
   
-  data = data[names(data) %nin% c(nzv.drop.factor, nzv.drop.numeric)]
+  ppobject$dropcols = c(nzv.drop.factor, nzv.drop.numeric)
+  ppobject$cols.factor = cols.factor
+  ppobject$cols.numeric = cols.numeric
+  
+  data = data[names(data) %nin% ppobject$dropcols]
 
   # univariate transformation (only on numerics)
-  if (univariate.trafo != "off") {
+  if (length(cols.numeric) > 0 && univariate.trafo != "off") {
     ndata = as.matrix(data[cols.numeric])
     if (univariate.trafo %in% c("center", "scale", "centerscale")) {
-      ndata = scale(ndata, center=center, scale=scale)
 
       center = univariate.trafo %in% c("center", "centerscale")
       scale = univariate.trafo %in% c("scale", "centerscale")
 
+      ndata = scale(ndata, center=center, scale=scale)
+
       ppobject$center = attr(ndata, "scaled:center")
       ppobject$scale = attr(ndata, "scaled:scale")
-
 
     } else {  # univariate.trafo == "range"
       rng = apply(ndata, 2, range, na.rm=TRUE)
@@ -62,59 +73,105 @@ preProcess = function(data, nzv.cutoff.numeric=0, nzv.cutoff.factor, univariate.
     }
     data[cols.numeric] = ndata
   }
-  
-  
-  removeNa = function(data, cols) {
-    delendum = Reduce(`&`, lapply(data[cols.numeric], is.na))
-    data[!delendum, ]
+
+  if (length(cols.numeric) > 0) {
+    if (impute.numeric == "remove.na") {
+      delendum = naRows(data, cols.numeric)
+      data = data[!delendum, ]
+      targetData = targetData[!delendum]
+    } else {
+      ppobject$pop.numeric = lapply(data[cols.numeric], function(x) {
+            switch(impute.numeric,
+            mean=mean(x),
+            median=median(x),
+            hist=x[!is.na(x)])
+          })  # TODO: maybe do rounding for mean / median and integers
+      data[cols.numeric] = mapply(imputeRandom, data[cols.numeric], ppobject$pop.numeric, SIMPLIFY=FALSE)
+    }
   }
   
-  imputeFun = function(x, fun, ...) {
-      x[is.na(x)] = fun(x[!is.na(x)], size=sum(is.na(x)), replace=TRUE)
-      x
+  if (length(cols.factor) > 0) {
+    if (impute.factor == "remove.na") {
+      delendum = naRows(data, cols.factor)
+      data = data[!delendum, ]
+      targetData = targetData
+    } else if (impute.factor == "distinct") {
+      data[cols.factor] = lapply(data[cols.factor], addNA, ifany=FALSE)
+    } else {
+      ppobject$pop.factor = lapply(data[cols.factor], function(x) {
+            switch(impute.factor, 
+            mode={
+              ftab = sort(table(x), TRUE)
+              names(ftab)[ftab==ftab[1]]  # if there is a tie, get all tieing levels
+            },
+            hist=x[!is.na(x)])
+          })
+      data[cols.factor] = mapply(imputeRandom, data[cols.factor], ppobject$pop.factor, SIMPLIFY=FALSE)
+    }
   }
 
-  switch(impute.numeric,
-      remove.na={
-        data = removeNa(data, cols.numeric)
-      },
-      mean={
-        data[cols.numeric] = lapply(data[cols.numeric], imputeFun, fun=mean)
-      },
-      median={
-        data[cols.numeric] = lapply(data[cols.numeric], imputeFun, fun=median)
-      },
-      hist={
-        data[cols.numeric] = lapply(data[cols.numeric], imputeFun, fun=sample)
-      })
-
-  switch(impute.factor,
-      remove.na={
-        data = removeNa(data, cols.factor)
-      },
-      distinct={
-        data[cols.factor] = lapply(data[cols.factor], addNA, ifany=TRUE)
-      },
-      mode={
-        data[cols.factor] = lapply(data[cols.factor], imputeFun, fun=function(x, ...) names(sort(table(x), TRUE))[1])
-      },
-      hist={
-        data[cols.factor] = lapply(data[cols.factor], imputeFun, fun=sample)
-      })
-
-  if (multivariate.trafo == "pca") {
-    pcr = prcomp(data[cols.numeric])
-    data[cols.numeric] = pcr$rotation
-  } else if (multivariate.trafo == "ica") {
-    requirePackages("fastICA", why="preProcess", default.method="load")
-    data[cols.numeric] = fastICA::fastICA(data[cols.numeric], length(cols.numeric),
-        method="C")$S
+  if (length(cols.numeric) > 0) {
+    if (multivariate.trafo == "pca") {
+      pcr = prcomp(data[cols.numeric], center=FALSE)
+      data[cols.numeric] = pcr$x
+      ppobject$rotation = pcr$rotation
+    } else if (multivariate.trafo == "ica") {
+      requirePackages("fastICA", why="preProcess", default.method="load")
+      ica = fastICA::fastICA(data[cols.numeric], length(cols.numeric), method="C")
+      data[cols.numeric] = ica$S
+      ppobject$rotation = ica$K %*% ica$W
+    }
   }
-  
+
+  if (DEBUG) {
+    ppobject$debugdata = data
+  }
+
+  ppobject
 }
 
-
-
 predict.ampreproc = function(object, newdata, ...) {
+  # all right lets go
+  # drop low variance cols
+  newdata = newdata[names(newdata) %nin% object$dropcols]
   
+  # univariate trafo
+  if (length(object$cols.numeric) > 0) {
+    if (object$univariate.trafo != "off") {
+      ndata = as.matrix(newdata[object$cols.numeric])
+      ndata = scale(ndata, center=coalesce(object$center, FALSE), scale=coalesce(object$scale, FALSE))
+      newdata[object$cols.numeric] = ndata
+    }
+    
+    if (object$impute.numeric == "remove.na") {
+      newdata = removeNa(newdata, object$cols.numeric)
+    } else {
+      newdata[object$cols.numeric] = mapply(imputeRandom, newdata[object$cols.numeric], object$pop.numeric, SIMPLIFY=FALSE)
+    }
+    
+    if (object$multivariate.trafo != "off") {
+      newdata[object$cols.numeric] = as.matrix(newdata[object$cols.numeric]) %*% object$rotation
+    }
+  }
+  
+  if (length(object$cols.factor) > 0) {
+    if (object$impute.factor == "remove.na") {
+      newdata = removeNa(newdata, object$cols.factor)
+    } else if (impute.factor == "distinct") {
+      newdata[object$cols.factor] = lapply(newdata[object$cols.factor], addNA, ifany=FALSE)
+    } else {
+      newdata[object$cols.factor] = mapply(imputeRandom, newdata[object$cols.factor], object$pop.factor, SIMPLIFY=FALSE)
+    }
+  }
+
+  newdata
+}
+
+naRows = function(data, cols) {
+  Reduce(`&`, lapply(data[cols], is.na))
+}
+
+imputeRandom = function(x, pop) {
+  x[is.na(x)] = sample(pop, size=sum(is.na(x)), replace=TRUE)
+  x
 }
--- working/working.R
+++ working/working.R
@@ -3,3 +3,13 @@ devtools::load_all("../../ParamHelpers")
 devtools::load_all("../../mlr")
 devtools::load_all("..")  # this veritable package itself
 
+
+ret <- preProcess(getTaskData(iris.task), univariate.trafo="centerscale", nzv.cutoff.numeric=0.5, multivariate.trafo="ica")
+all.equal(predict(ret, getTaskData(iris.task)), ret$debugdata)
+
+ret$scale
+ret$center
+dat <- getTaskData(iris.task)
+dat[ret$cols.numeric] <- as.matrix(getTaskData(iris.task)[ret$cols.numeric]) %*% ret$rotation
+ret$rotation
+dat
