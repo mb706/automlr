e9e022735530025be9a3c37ddc4acbb4e05cfd45
mb706 <ma.binder@campus.lmu.de>
2017-07-16 18:36:29 -0400

    Prevent segfault caused by java. This helps only if automlr gets loaded before java packages get loaded.

--- R/zzz.R
+++ R/zzz.R
@@ -9,14 +9,18 @@ NULL
 mlr.predictLearner.ModelMultiplexer = NULL
 
 .onLoad = function(libname, pkgname) {
-  
+
   mlrWrappers <<- mlrWrappers.gen()
   mlrLearners <<- mlrLearners.gen()
   mlrLightweight <<- mlrLightweight.gen()
-  
+
   mlr.predictLearner.ModelMultiplexer <<- mlr:::predictLearner.ModelMultiplexer
 
   timeoutMessage <<- determineTimeoutMessage()
-  
-  
+
+
+  # TODO: this is necessary for some linux kernels to prevent segfaults.
+  options(java.parameters = c("-Xss2560k", "-Xmx2g"))
+
+
 }
