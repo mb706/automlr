56d3855b77145d8f3a7af0368eaa826558c7ef1d
mb706 <ma.binder@campus.lmu.de>
2017-07-17 16:32:53 -0400

    Correcting test files

--- working/runmbo.R
+++ working/runmbo.R
@@ -1,6 +1,6 @@
 library("automlr")
 library("mlrMBO")
 options(error=dump.frames)
-amrun = automlr(pid.task, backend = "mbo", budget = c(walltime = 3600 * 7), max.walltime.overrun=Inf, verbosity = 5, max.learner.time=500, searchspace=mlrLightweight[1:53],
+amrun = automlr(pid.task, backend = "mbo", budget = c(walltime = 3600 * 7), max.walltime.overrun=Inf, verbosity = 5, max.learner.time=500, searchspace=mlrLightweight,
 save.interval = 1200, savefile = "./")
 
