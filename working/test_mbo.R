d8ab2df979a09a56ce4434d87d9c0dd4a9a11110
mb706 <ma.binder@campus.lmu.de>
2017-07-17 16:31:30 -0400

    test files

---
+++ working/test_mbo.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+argv = as.numeric(commandArgs(trailingOnly = TRUE)[1])
+library("methods")
+library("automlr")
+
+
+
+system.time(automlr(pid.task,
+  budget = c(walltime = 3600 * 7), verbosity=5, save.interval = 600,
+  savefile = "./", max.learner.time = 1200,
+  searchspace = mlrLightweightNoWrap, backend = "mbo"),
+  FALSE)
+
+cat("Finished successfully\n")
